8a1c9592c9147dfd3b431c757dd4c35e2a3e792e
L. Morrill <lm687@login-e-12.data.cluster>
2020-06-14 09:18:00 +0100

    automated message

--- code/wrap_inference_hcp_DM.py
+++ code/wrap_inference_hcp_DM.py
@@ -7,9 +7,9 @@ import os
 
 nits = 20000
 for i in ['../data/roo/'+x for x in os.listdir("../data/roo/") if 'ROO' in x]:
-    script = "~/.conda/envs/rstan_env/bin/Rscript --vanilla 2_inference/fit_PCAWG.R --cancertype {} --typedata {} --infile {} --output {} --iterations {} --model M".format(i.split('_')[0], (i.split('_')[1]), i, i.replace("roo/", "inference/").replace("_ROO", "_"+str(nits)+"ROO").replace(".RDS", ".RData"), str(nits))
+    script = "~/.conda/envs/rstan_env/bin/Rscript --vanilla 2_inference/fit_PCAWG.R --cancertype {} --typedata {} --infile {} --output {} --iterations {} --model DM".format(i.split('_')[0], (i.split('_')[1]), i, i.replace("roo/", "inference/").replace("_ROO", "_"+str(nits)+'_'+'DM'+"ROO").replace(".RDS", ".RData"), str(nits))
 
-    fileout="sbatches/"+os.path.basename(i).replace(".RDS", "_v2.sh")
+    fileout="sbatches/"+os.path.basename(i).replace(".RDS", "_DM.sh")
     a = open(fileout, "w")
     a.write('''#!/bin/sh
 #SBATCH --job-name=Stan_R{}
@@ -34,7 +34,6 @@ conda deactivate
     print("sbatch "+fileout)
 
 
-#Rscript --vanilla 2_inference/fit_PCAWG.R --cancertype Prost-AdenoCA --typedata signatures         --infile ../data/roo/Prost-AdenoCA_signatures_ROO.RDS --output ../data/inference/M_6000_Prost-AdenoCA_signatures.Rdata --niterations 6000 --model M
 
 
 
