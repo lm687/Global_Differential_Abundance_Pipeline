350f4f796c3c563454b5f0ee6acf6949751bcffc
L. Morrill <lm687@login-e-9.data.cluster>
2020-09-01 09:41:51 +0100

    tidy up slurm

---
+++ code/.snakemake/tmp.zcb10a34/snakejob.inference.106.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# properties = {"type": "single", "rule": "inference", "local": false, "input": ["../data/roo/Ovary-AdenoCA_nucleotidesubstitution3_ROO.RDS"], "output": ["../data/inference/Ovary-AdenoCA_nucleotidesubstitution3_15000_MROO.RData"], "wildcards": {"cancer_type": "Ovary-AdenoCA", "feature_type": "nucleotidesubstitution3", "nits": "15000", "model": "M"}, "params": {"cancer_type": "Ovary-AdenoCA", "feature_type": "nucleotidesubstitution3", "model": ["M"], "nits": ["15000"]}, "log": ["logs/inference/Ovary-AdenoCA_nucleotidesubstitution3_15000_M.log"], "threads": 1, "resources": {}, "jobid": 106, "cluster": {}}
+ cd /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code && \
+/home/lm687/.conda/envs/snakemake-globalDA/bin/python3.8 \
+-m snakemake ../data/inference/Ovary-AdenoCA_nucleotidesubstitution3_15000_MROO.RData --snakefile /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code/Snakefile \
+--force -j --keep-target-files --keep-remote \
+--wait-for-files /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code/.snakemake/tmp.zcb10a34 ../data/roo/Ovary-AdenoCA_nucleotidesubstitution3_ROO.RDS --latency-wait 5 \
+ --attempt 1 --force-use-threads \
+--wrapper-prefix https://github.com/snakemake/snakemake-wrappers/raw/ \
+  -p --allowed-rules inference --nocolor --notemp --no-hooks --nolock \
+--mode 2  && touch /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code/.snakemake/tmp.zcb10a34/106.jobfinished || (touch /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code/.snakemake/tmp.zcb10a34/106.jobfailed; exit 1)
+
--- code/inference_summary.py
+++ code/inference_summary.py
@@ -5,6 +5,9 @@
 import os
 import re
 import numpy as np
+from datetime import datetime, date
+
+months = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
 
 fles = [i for i in os.listdir() if "slurm-" in i]
 
@@ -15,19 +18,28 @@ def to_float_time2(i):
 	# (re.sub(r' seconds.*', '', re.sub(r'^.*?: ', '', i), flags=re.DOTALL))
 	# (re.sub(r' seconds.*', '',i, flags=re.DOTALL))
 
+def give_datetime(data, hms):
+	if(len(data) == 5): ## short version
+       		return(datetime(year=int(data[4]), month=months[data[1]], day=int(data[2]), hour=int(hms[0]), minute=int(hms[1]), second=int(hms[2])))
+	elif(len(data) == 6): ## 6 char; there is a space
+                return(datetime(year=int(data[5]), month=months[data[1]], day=int(data[3]), hour=int(hms[0]), minute=int(hms[1]), second=int(hms[2])))
+	else:
+		exit()
+
 # print("00 CT \t features \t divergent transitions \t Rhat high \t ESS")
-print("*slurm,CT,features,nits,model,divergent transitions,Rhat high,ESS,Cancelled (time limit), warmup time (s), sampling time (s), total time (s)")
+print("0timestamp, slurm,CT,features,nits,model,divergent transitions,Rhat high,ESS,Cancelled (time limit), warmup time (s), sampling time (s), total time (s), real time (s)")
 for fle in fles:
 	# print(fle)
 	list_outfile = None; #line_elapsed_time = None;
-	time_limit = '.'; div_trans = '.'; Tail_ESS = '-'; Rhat = '-'
+	time_limit = ''; div_trans = ''; Tail_ESS = ''; Rhat = ''
 	a = open(fle, "r")
 	# contents = a.read()
 
 	# print('Elapsed' in contents)
 
 	## search lines
-	warm_up_time = []; sampling_time = []; total_time = []
+	warm_up_time = []; sampling_time = []; total_time = []; dates = []
+	timestamp = None
 	for line in a:
 		if re.search("output\:", line):
 			list_outfile = os.path.basename(line).rstrip("ROO.RData\n").split('_')
@@ -39,43 +51,56 @@ for fle in fles:
 			Rhat = True
 		if re.search("DUE TO TIME LIMIT", line):
 			time_limit = True
-		# if re.search("Elapsed Time", line):
-		# 	line_elapsed_time.append(line)
+		if re.search("2020", line) and re.search(r"\[*\]", line):
+			timestamp = line.strip('\n')
 		if re.search("seconds \(Warm-up\)", line):
 			warm_up_time.append(line)
 		if re.search("seconds \(Sampling\)", line):
 			sampling_time.append(line)
 		if re.search("seconds \(Total\)", line):
 			total_time.append(line)
+		if re.search("2020", line):
+			if not re.search("slurmstepd", line):
+				dates.append(line)
 
-
-	# print([ to_float_time(i) for i in warm_up_time])
-	
 	warm_up_time = np.array([ to_float_time(i) for i in warm_up_time]).astype(np.float)
-	# print(np.mean(warm_up_time))
 	sampling_time = np.array([ to_float_time2(i) for i in sampling_time]).astype(np.float)
-	# print(np.mean(sampling_time))
 	total_time = np.array([ to_float_time2(i) for i in total_time]).astype(np.float)
-	# print(np.mean(total_time))
 
-	# print(total_time is None)
-	# print(len(total_time))
-	# if not np.isnan(total_time):
 	if len(total_time > 0):
-		# times_avg = '\t'.join([str(np.mean(warm_up_time)), str(np.mean(sampling_time)), str(np.mean(total_time))])
-		# print('\t'.join([str(np.mean(warm_up_time)), str(np.mean(sampling_time))]))
-		times_avg = ','.join([str(np.round(np.mean(warm_up_time))),\
-		 str(np.round(np.mean(sampling_time))), str(np.round(np.mean(total_time)))])
+		times_avg = ','.join([str(np.round(np.mean(warm_up_time))), str(np.round(np.mean(sampling_time))), str(np.round(np.mean(total_time)))])
+		# print('\t'.join([str(np.mean(warm_up_time)), str(np.mean(sampling_time))]))		times_avg = ','.join([str(np.round(np.mean(warm_up_time))),\
+		 # str(np.round(np.mean(sampling_time))), str(np.round(np.mean(total_time)))])
 	else:
-		times_avg = ','.join(['-', '-', '-'])
+		times_avg = ','.join(['', '', ''])
 
-	# print(times_avg)
-	# print(a[line_elapsed_time]) ## warmup
-	# print(a[line_elapsed_time+1]) ## sampling
-	# print(a[line_elapsed_time+2]) ## total
-	# print([x for i, x in enumerate(a) if i in line_elapsed_time])
-	# print("%s \t %s \t %s \t %s" % ('\t'.join(list_outfile), div_trans, Rhat, Tail_ESS))
-	print("%s,%s,%s,%s,%s,%s,%s" % (fle, ','.join(list_outfile), div_trans, Rhat, Tail_ESS, time_limit, times_avg))
+	if(len(dates) == 1):
+		real_time = ''		
+	else:
+		#print((dates))
+		a = dates[0] #"[Sun Aug  2 16:46:45 2020]"
+		b = dates[1] #"[Sun Aug  2 16:47:34 2020]"
+	
+		dataa = (a.strip('\n').strip('[|]').split(" "))
+		if(len(dataa) == 5):
+			hmsa = dataa[3].split(":")
+		else:
+			hmsa = dataa[4].split(":")
+	
+		datab = (b.strip('\n').strip('[|]').split(" "))
+		if(len(datab) == 5):
+			hmsb = datab[3].split(":")
+		else:
+			hmsb = datab[4].split(":")
+
+		#print(dataa)
+		#print(fle)
+		#print(hmsa)	
+		a_datetime = give_datetime(dataa, hmsa)
+		b_datetime = give_datetime(datab, hmsb)
+		real_time = str( int ((b_datetime-a_datetime).total_seconds()) )
+
+	print("%s,%s,%s,%s,%s,%s,%s,%s,%s" % (timestamp,fle, ','.join(list_outfile), div_trans, Rhat, Tail_ESS, time_limit, times_avg, real_time))
 
 
 
