0fbd90242d7d222991e3adcc77a8be6dfeced713
Lena Morrill <lm687@cam.ac.uk>
2021-11-25 14:18:10 +0000

    bias

---
+++ code/.snakemake/tmp.8n_d2xv1/snakejob.simulate_for_comparison_multiple.1808.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# properties = {"type": "single", "rule": "simulate_for_comparison_multiple", "local": false, "input": ["../data/assessing_models_simulation/GenerationJnorm"], "output": ["../data/assessing_models_simulation/datasets/multiple_GenerationJnorm_200_180_100_5_0_betaintercept1d4_betaslopecov1d4_cov1d4_dataset807.RDS"], "wildcards": {"datasetgeneration": "GenerationJnorm", "n": "200", "nlambda": "180", "lmbda": "100", "d": "5", "beta_intensity": "0", "fixed_beta_intercept": "betaintercept1d4", "fixed_beta_slope": "betaslopecov1d4", "sdRE_input": "cov1d4", "itnum": "807"}, "params": {"datasetgeneration": "GenerationJnorm", "n": "200", "nlambda": "180", "lmbda": "100", "d": "5", "beta_intensity": "0", "itnum": "807", "fixed_beta_intercept": "betaintercept1d4", "fixed_beta_slope": "betaslopecov1d4", "sdRE_input": "cov1d4"}, "log": [], "threads": 1, "resources": {}, "jobid": 1808, "cluster": {}}
+ cd /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code && \
+/home/lm687/.conda/envs/snakemake-globalDA/bin/python3.8 \
+-m snakemake ../data/assessing_models_simulation/datasets/multiple_GenerationJnorm_200_180_100_5_0_betaintercept1d4_betaslopecov1d4_cov1d4_dataset807.RDS --snakefile /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code/Snakefile \
+--force -j --keep-target-files --keep-remote \
+--wait-for-files /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code/.snakemake/tmp.8n_d2xv1 ../data/assessing_models_simulation/GenerationJnorm --latency-wait 5 \
+ --attempt 1 --force-use-threads \
+--wrapper-prefix https://github.com/snakemake/snakemake-wrappers/raw/ \
+  -p --allowed-rules simulate_for_comparison_multiple --nocolor --notemp --no-hooks --nolock \
+--mode 2  && touch /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code/.snakemake/tmp.8n_d2xv1/1808.jobfinished || (touch /rds/user/lm687/hpc-work/Global_Differential_Abundance_Pipeline/code/.snakemake/tmp.8n_d2xv1/1808.jobfailed; exit 1)
+
--- code/3_analysis/simulation_model_assessment/generate_datasets_simulations/GenerationJnorm.R
+++ code/3_analysis/simulation_model_assessment/generate_datasets_simulations/GenerationJnorm.R
@@ -73,7 +73,8 @@ if(is.null(opt$beta_intercept_input)){
       sim_beta_1 = T
     }else{   
       cat('Reading input beta intercept file')
-      beta[1,] = readRDS(opt$beta_intercept_input)
+      print(opt$beta_intercept_input) 
+     beta[1,] = readRDS(opt$beta_intercept_input)
       sim_beta_1 = F
     }
   }
@@ -95,6 +96,7 @@ if(is.null(opt$beta_slope_input)){
     }else{
       sim_beta_2 = F
       cat('Reading input beta slope file')
+      print(opt$beta_slope_input)
     read_beta_slope = readRDS(opt$beta_slope_input)
     stopifnot( (opt$d-1) == length(read_beta_slope))
     beta[2,] = read_beta_slope
